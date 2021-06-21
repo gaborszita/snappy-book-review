1cf543002b5a3578fbee5e7c83b235bd889361ff
Gabor Szita <gaborszita@yahoo.com>
2021-06-21 15:06:43 +0100

    Place curly braces. (Before curly braces were ommited.)

--- public_html/account/log-out/submit/index.php
+++ public_html/account/log-out/submit/index.php
@@ -1,10 +1,12 @@
 <?php
 require_once('../../../../resources/config.php');
 
-if (isset( $_COOKIE[session_name()]))
+if (isset( $_COOKIE[session_name()])) {
     setcookie( session_name(), "", time()-3600, "/");
+}
 
-if(isset($_SESSION))
+if(isset($_SESSION)) {
     session_destroy();
+}
 
 echo "Success!";
