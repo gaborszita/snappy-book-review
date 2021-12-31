24f73b673738b0b24e97f222ffcb172b6d1d0f06
Gabor Szita <gaborszita88@gmail.com>
2021-12-31 19:08:15 +0100

    Update config model to include new fields and
    Four new fields were added to the config model: sessionSecret,
    sessionMaxAge, sessionCookie, and loggedInCookie.
    sessionSecret and sessionMaxAge is customizeable (i.e. is actually
    stored in database); sessionCookie and loggedInCookie is hard-coded in
    the config model (i.e. in virtual getter funtions)

--- src/models/Config.ts
+++ src/models/Config.ts
@@ -4,19 +4,31 @@ export interface IConfig extends Document {
   sitePreferredProtocol: string,
   siteAddress: string,
   siteUrl: string,
-  sessionSecret: string
+  sessionSecret: string,
+  sessionMaxAge: number,
+  sessionCookie: string,
+  loggedInCookie: string
 }
 
 const ConfigSchema = new Schema<IConfig>({
   sitePreferredProtocol: { type: String, requiured: true },
   siteAddress: { type: String, required: true },
-  sessionSecret: { type: String, required: true }
+  sessionSecret: { type: String, required: true },
+  sessionMaxAge: { type: Number, required: true }
 }, {
   capped: { size: 1024, max: 1 }
 });
 
 ConfigSchema.virtual('siteUrl').get(function() {
   return this.sitePreferredProtocol + '://' + this.siteAddress
+});
+
+ConfigSchema.virtual('sessionCookie').get(function() {
+  return 'session_id';
+})
+
+ConfigSchema.virtual('loggedInCookie').get(function() {
+  return 'logged_in';
 });
 
 export const Config = mongoose.model<IConfig>('config', ConfigSchema);
