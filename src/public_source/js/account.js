4624b2de8974645943e33198cba221f2d6b92bc3
Gabor Szita <gaborszita88@gmail.com>
2022-01-01 19:04:00 +0100

    Remove HTMLStyleElement.type from account.js
    Removed HTMLStyleElement.type from account.js, because this feature is
    deprecated.

--- src/public_source/js/account.js
+++ src/public_source/js/account.js
@@ -1,20 +1,17 @@
 export class Account {
   static checkLogin() {
     const defaultStylesheet = document.createElement('style');
-    defaultStylesheet.type = 'text/css';
     defaultStylesheet.innerText = '#login_button, #signup_button, #logout_button { display: none; }';
     document.head.appendChild(defaultStylesheet);
 
     const cookieName = '<%= config.loggedInCookie %>';
     if (document.cookie.match('(^|;)\\s*' + cookieName + '\\s*=\\s*([^;]+)')?.pop() === 'true') {
       const stylesheet = document.createElement('style');
-      stylesheet.type = 'text/css';
       stylesheet.innerText = '#logout_button { display: list-item; }';
       document.head.appendChild(stylesheet);
       return true;
     } else {
       const stylesheet = document.createElement('style');
-      stylesheet.type = 'text/css';
       stylesheet.innerText = '#login_button, #signup_button { display: list-item; }';
       document.head.appendChild(stylesheet);
       return false;
