232def474fe7947dc920d9567420ae3ff7765bd0
Gabor Szita <gaborszita88@gmail.com>
2022-06-22 17:12:58 +0200

    Add trailing slash to book routes

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -7,16 +7,16 @@ export const router = Router();
 router.get('/search/', books.search);
 
 // GET request for a book's page
-router.get('/book/:bookISBN', books.book);
+router.get('/book/:bookISBN/', books.book);
 
 // GET request for post review page
-router.get('/books/post-review', books.postReview);
+router.get('/books/post-review/', books.postReview);
 
 // POST request for post review submit
-router.post('/books/post-review/submit', books.postReviewSubmit);
+router.post('/books/post-review/submit/', books.postReviewSubmit);
 
 // GET request for isbn validator
-router.get('/books/isbn-validator', books.isbnValidator);
+router.get('/books/isbn-validator/', books.isbnValidator);
 
 // POST request for delete review submit
-router.post('/books/delete-review/submit', books.deleteReviewSubmit);
+router.post('/books/delete-review/submit/', books.deleteReviewSubmit);
