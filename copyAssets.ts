a8f8993543f9975da115a7a3bfcd02d982d58b0f
Gabor Szita <gaborszita88@gmail.com>
2022-06-28 10:22:12 -0700

    Add robots.txt

--- copyAssets.ts
+++ copyAssets.ts
@@ -2,6 +2,6 @@ import * as shell from 'shelljs';
 
 shell.cp('-R', 'src/views', 'dist/views');
 shell.cp('-R', 'src/public/img', 'dist/public/img');
-shell.cp('src/public/favicon.ico', 'dist/public/favicon.ico')
+shell.cp('-R', 'src/public/root/.', 'dist/public');
 shell.cp('-R', 'node_modules/@fortawesome/fontawesome-free/webfonts',
          'dist/public');
